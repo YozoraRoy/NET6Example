6bcbffd3926510c44708802228bfc937ffed1dde
Roy <yuzora_roy@hotmail.com>
2022-07-16 22:20:32 +0900

    all file upload

---
+++ WebApplication_example_NET6_dapper/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication_example_NET6_dapper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
