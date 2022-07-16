870a72c693bf27003980e3a7c48e89e8cd3c7578
Roy <yuzora_roy@hotmail.com>
2022-07-16 23:29:57 +0900

    update all file

--- WebApplication_example_NET6_dapper/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication_example_NET6_dapper.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
